4b6c8aae59cd74262a17feff38a642a91b7bff34
Ustinova Ekaterina <galateya2406@yandex.ru>
2023-01-20 22:17:40 +0400

    Add new practice

---
+++ Student's Final Grade.js
@@ -0,0 +1,12 @@
+function finalGrade (exam, projects) {
+  let finalGrade = 0;
+  
+  if (exam > 90 || projects > 10) {
+    finalGrade = finalGrade + 100;
+  } else if (exam > 75 && projects >= 5) {
+    finalGrade = finalGrade + 90;
+  } else if (exam > 50 && projects >= 2) {
+    finalGrade = finalGrade + 75;
+  } else {
+    finalGrade = finalGrade;
+  }
