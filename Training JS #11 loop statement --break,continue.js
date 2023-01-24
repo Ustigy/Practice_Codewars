28fc1a9da128d21ab87bcf552c757e01fce32906
Ustinova Ekaterina <galateya2406@yandex.ru>
2023-01-24 14:52:28 +0400

    Add new practice

---
+++ Training JS #11 loop statement --break,continue.js
@@ -0,0 +1,17 @@
+function grabDoll(dolls){
+  var bag=[];
+  
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty"
+    || dolls[i] === "Barbie doll") {
+      
+      if (bag.length < 3) {
+        bag.push(dolls [i]);
+      } else break;
+      
+    } else continue;
+    
+  }
+
+  return bag;
+}
