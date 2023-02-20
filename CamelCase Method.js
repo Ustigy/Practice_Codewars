e7349d66acf3be1d73313d39772cf1b07fa14715
Ustinova Ekaterina <galateya2406@yandex.ru>
2023-02-20 20:00:25 +0400

    Add a new task

--- CamelCase Method.js
+++ CamelCase Method.js
@@ -1,11 +1,21 @@
 String.prototype.camelCase=function(){
   let array = this.split(' ');
   let camelArray = [];
+  console.log(array)
   
   for (let key of array) {
+    console.log(key == 0)
     if (key == 0) continue;
     camelArray += key[0].toUpperCase() + key.slice(1, key.length);
-   }
+    
+  }
   
+//   console.log(camelArray);
   return (String(camelArray));
 }
+
+//   console.log("test case".camelCase())
+//   console.log("camel case method".camelCase())
+// //   console.log("say hello".camelCase())
+//   console.log(" camel case word".camelCase())
+//   console.log("".camelCase())
