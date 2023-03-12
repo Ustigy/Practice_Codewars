79686beb52de7787d19d2bbaf13b36e57052e343
Ustinova Ekaterina <galateya2406@yandex.ru>
2023-03-12 17:01:41 +0400

    Add a new task

--- Array.diff.js
+++ Array.diff.js
@@ -1,10 +1,7 @@
 function arrayDiff(a, b) {
-
-  let arrayDiff;
   for (let valueB of b) {
-    if (valueB === undefined) return a;
-    arrayDiff = a.filter(valueA => valueA !== valueB)
+    a = a.filter(valueA => valueA !== valueB);
   }  
   
-  return arrayDiff;
+  return a;
 }
