e522ff496ca1d2728f2c7e90be9ded55c8b7ab55
Ustinova Ekaterina <galateya2406@yandex.ru>
2023-03-24 11:43:12 +0400

    Add a new task

--- Don't give me five!.js
+++ Don't give me five!.js
@@ -1,41 +1,15 @@
 let dontGiveMeFive = (start, end) => {
-  console.log(start, end)
-  let row = '';
+
+  let row = [];
+  for (let i = start; i <= end; i++){
+    row.push(i);
+  }
+
+  let rowFiltered = [];
+  for (let prop of row) {
+    let valueFiltered = String(prop).split('').filter(el => el != 5).join('');
+    if (+valueFiltered === prop) rowFiltered.push(prop);
+  }
   
-  for (let i = start; i < end; i++) {
-    row += i + ',';
-  } row = row + end;
-  row = row.split(',');
-  console.log(row);
-   
-  let firstFive = 0;
-  for (let key of row) {
-    if (key % 5 !== 0 || key % 10 === 0) {
-      firstFive = Number(key) + 1;
-    } else {
-      break;
-    }
-  }
-  console.log(firstFive)
-
-  let quantityFive = 0;
-  for (let j = firstFive; j <= end; j += 10) {
-    quantityFive++;
-  }
-     console.log(quantityFive) 
-     console.log(row.length - quantityFive)
-  return (row.length - quantityFive)
+  return rowFiltered.length;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
