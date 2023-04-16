9fe0ffe97a340089b3580c845e36f700288432df
Ustinova Ekaterina <galateya2406@yandex.ru>
2023-04-16 15:27:20 +0400

    Add a new task

--- Equal Sides Of An Array.js
+++ Equal Sides Of An Array.js
@@ -1,17 +1,25 @@
 function findEvenIndex(arr) {
-  let sumLeft = 0;
-  let sumRight = 0;
   
-  for (let i = 0; i < arr.length; i++) {
-    sumLeft += arr[i];
-    
-    while (sumLeft !== sumRight) {
-      for (let j = -1; j >= -arr.length; j--) {
-        sumRight += arr.at(j);
-      }
-    }
-  
+  if (arr.reduce((sum, el) => sum + el, 0) === 0) {
+    return 0;
   }
   
-  return sumRight
+  let i;
+  for (i = 1; i < arr.length; i++) {
+    
+    let leftSum = 0;
+    for (let jLeft = 0; jLeft < i; jLeft++) {
+      leftSum += arr[jLeft];
+    }
+    
+    let rightSum = 0;
+    for (let jRight = arr.length - 1; jRight > i; jRight--) {
+      rightSum += arr[jRight];
+    }
+
+    if (leftSum === rightSum) break;
+    if (rightSum === 0 && leftSum !== rightSum) i = -1;
+  }
+
+    return i;
 }
